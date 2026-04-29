5b63eefd1621138869741b180f7eff6ddc825685
bsc-com-04-24 <bsc-com-04-24@unima.ac.mw>
2026-04-28 22:39:14 -0700

    running main2

--- Lab9/Polygon/main2.cpp
+++ Lab9/Polygon/main2.cpp
@@ -4,15 +4,24 @@
 
 using namespace std;
 
-int main(){
+int main2(){
     Rectangle2 rect;
     Triangle2 trgl;
+    Polygon poly;
 
     Polygon* pPoly1 = &rect;       //pointer of type baseclass has address of a derived class' object
     Polygon* pPoly2 = &trgl;
+    Polygon* pPoly = &poly;
     
     pPoly1 -> SetValues(4,5);       //equivalent to rect.SetValues(4,5)
     pPoly2 -> SetValues(4,5);
+    pPoly -> SetValues(4,5);
+
+    cout << pPoly1->Area() << '\n';
+    cout << pPoly1->Area() << '\n';
+    cout << pPoly->Area() << '\n';
+
+    return 0;
 }
 
 
